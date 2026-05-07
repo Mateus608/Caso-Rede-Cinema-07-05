3fd1fc60ce94ac706f14cd4c85f50a8401e18a9c
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2026-05-07 20:45:59 -0300

    Add Sessao class with initialization parameters

---
+++ src/controller/model/sessao.py
@@ -0,0 +1,6 @@
+class Sessao:
+
+    def __init__(self, horario_inicio, horario_fim, publico=0):
+        self.horario_inicio = horario_inicio
+        self.horario_fim = horario_fim
+        self.publico = publico
