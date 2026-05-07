1d4c805556ca58882852e964faf6c90e9dadc160
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2026-05-07 20:50:35 -0300

    Implement SessaoService with session registration

---
+++ src/service/sessao_service.py
@@ -0,0 +1,16 @@
+from repository.sessao_repository import SessaoRepository
+
+class SessaoService:
+
+    def __init__(self):
+        self.repository = SessaoRepository()
+
+    def cadastrar_sessao(self, sessao):
+
+        if not sessao.horario_inicio:
+            raise Exception("Horário inicial inválido")
+
+        if not sessao.horario_fim:
+            raise Exception("Horário final inválido")
+
+        self.repository.salvar(sessao)
